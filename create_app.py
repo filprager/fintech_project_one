5a2ab320e10d0a2680d20a33273662ad31a46243
Raj Rajarajan <raj.rajarajan@team.telstra.com>
2021-01-06 21:29:59 +1100

    Flask updates - run create_app function

--- create_app.py
+++ create_app.py
@@ -12,30 +12,30 @@ from make_interface import make_interface
 
 def create_app():
     print('create_app get it working')
-    # Check if the sum of all weights = 1
-    if weight1 + weight2 != 1:
-        return 'Make sure the sum of your weights is 1'
+    # # Check if the sum of all weights = 1
+    # if weight1 + weight2 != 1:
+    #     return 'Make sure the sum of your weights is 1'
 
-    # --------------------- Get the data from APIs ---------------------
-    # Get data for given stocks
-    tickers = [ticker1, ticker2]
-    raw_data_stock = get_data_stock(tickers)
+    # # --------------------- Get the data from APIs ---------------------
+    # # Get data for given stocks
+    # tickers = [ticker1, ticker2]
+    # raw_data_stock = get_data_stock(tickers)
 
-    # Get data of the market (i.e. S&P 500 index)
-    raw_data_market = get_data_market()
+    # # Get data of the market (i.e. S&P 500 index)
+    # raw_data_market = get_data_market()
 
-    # --------------------- Process the data ---------------------
-    tickers = [ticker1, ticker2]
-    weights = [weight1, weight2]
-    processed_data_stock = process_data(raw_data_stock, tickers, weights)
+    # # --------------------- Process the data ---------------------
+    # tickers = [ticker1, ticker2]
+    # weights = [weight1, weight2]
+    # processed_data_stock = process_data(raw_data_stock, tickers, weights)
 
-    # --------------------- Visualize the data ---------------------
-    plot1 = make_plot1(processed_data_stock)
-    plot2 = make_plot2(processed_data_stock)
+    # # --------------------- Visualize the data ---------------------
+    # plot1 = make_plot1(processed_data_stock)
+    # plot2 = make_plot2(processed_data_stock)
 
-    # --------------------- Make the interface of the app ---------------------
-    app = make_interface(plot1, plot2)
+    # # --------------------- Make the interface of the app ---------------------
+    # app = make_interface(plot1, plot2)
 
-    return app
+    # return app
 
-    # return 'my .py is working '
+    return 'my .py is working '
--- flaskinput.py
+++ flaskinput.py
@@ -8,40 +8,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '00e4efeb013b5fc6ce708590b1a4d6a3'
 
 
-def my_func():
-    my_num = 6
-    return my_num
-
-
 @app.route('/')
 @app.route('/about')
 def about():
-    my_func
-    print('testing')
+
     return render_template('about.html', title='About')
 
 
 @app.route('/interface', methods=['GET', 'POST'])
 def interface():
-    # def my_func():
-    #     my_num = 4
-    #     return my_num
-
     form = PortfolioForm()
     if form.validate_on_submit():
-        my_num = 2
-        print(my_num)
-
         flash(f'Inital Investment: {form.username.data}!', 'success')
         flash(f'ticker1: {form.ticker1.data}', 'success')
         flash(f'ticker2: {form.ticker2.data}', 'success')
         flash(f'ticker3: {form.ticker3.data}', 'success')
         flash(f'ticker4: {form.ticker4.data}', 'success')
-        flash(f'show my function: {create_app()}')
         # createapp
         # output = create_app('MSFT', 'DIS', 0.5, 0.5)
-        # create_app()
-        my_func
+        create_app()
         return redirect(url_for('about'))
     return render_template('interface.html', title='Ticker', form=form)
 
