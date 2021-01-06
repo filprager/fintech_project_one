a67271242a3bd93d4e87fce41fe9751417b0e187
filprager <filprager@gmail.com>
2021-01-06 18:49:19 +1100

    Added Monte Carlo

--- make_interface.py
+++ make_interface.py
@@ -1,9 +1,19 @@
 import panel as pn
 
-def make_interface(plot1, plot2):
+def make_interface(plot1, plot2, plot3, plot4):
+    
+    welcome_panel = pn.Column(
+    "# Welcome to DeLorean",
+    "### This financial analysis tool presents a visual analysis of historical stock ticker performance for a selected date range, as well as predicted future performance using Monte Carlo analysis",
+    "### You can navigate through the tabs above to explore more detail about your selected stock tickers"
+    )
+    
     tabs = pn.Tabs(
-        ('Tab1', plot1),
-        ('Tab2', plot2)
+        ("Welcome", welcome_panel),
+        ('Historical - Line', plot1),
+        ('Historical - Bar', plot2),
+        ('Projected - Monte Carlo', plot3),
+        ('Projected - Distribution', plot4)
     
     )
     
